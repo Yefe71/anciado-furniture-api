3c82fe762995d89bac7bb1d0408d086d0b8dc99c
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-07-01 20:52:47 +0800

    api call returning thank God

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,38 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({strapi}) => ({
+    handleWebhook: async (ctx) => {
+        try {
+          // Extract the event data from the request body
+        //   const eventData = ctx.request.body;
+    
+        //   // Process the event based on its type
+        //   switch (eventData.data.attributes.type) {
+        //     case 'payment.paid':
+        //       // Logic to handle payment.paid event
+        //       break;
+        //     case 'payment.failed':
+        //       // Logic to handle payment.failed event
+        //       break;
+        //     case 'source.chargeable':
+        //       // Logic to handle source.chargeable event
+        //       break;
+        //     default:
+        //       // Unknown event type
+        //       break;
+        //   }
+    
+        //   console.log(eventData, "PLEASE I RAN RIGHT?")
+        //   // Send a successful response to PayMongo
+        //   ctx.send({ received: true });
+
+        console.log("I RAN RIGHT?")
+        return 1;
+        } catch (error) {
+        //   // Handle any errors that occur during event processing
+        //   console.error(error);
+        //   ctx.throw(500, 'Error processing webhook event');
+        }
+      },
+}) );
