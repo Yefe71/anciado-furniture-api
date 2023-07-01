f9d706182f7772475225b0ad2b387959ee97be0a
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-07-01 23:28:40 +0800

    just optimization in order

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -11,32 +11,13 @@ module.exports = createCoreController('api::order.order', ({strapi}) => ({
         try {
     
           const eventData = ctx.request.body;
-    
-        //   // Process the event based on its type
-        //   switch (eventData.data.attributes.type) {
-        //     case 'payment.paid':
-        //       // Logic to handle payment.paid event
-        //       break;
-        //     case 'payment.failed':
-        //       // Logic to handle payment.failed event
-        //       break;
-        //     case 'source.chargeable':
-        //       // Logic to handle source.chargeable event
-        //       break;
-        //     default:
-        //       // Unknown event type
-        //       break;
-        //   }
-    
-          console.log(eventData, "PLEASE I RAN RIGHT?")
-        //   // Send a successful response to PayMongo
-        //   ctx.send({ received: true });
-
-            // return ctx.request.body;
+          ctx.status = 200;
+          ctx.body = 'Webhook event processed successfully';
+          console.log(eventData)
         } catch (error) {
-        //   // Handle any errors that occur during event processing
-        //   console.error(error);
-        //   ctx.throw(500, 'Error processing webhook event');
+          // Handle any errors that occur during event processing
+          console.error(error);
+          ctx.throw(500, 'Error processing webhook event');
         }
       },
 }) );
